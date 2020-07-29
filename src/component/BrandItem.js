6de8fa83dd34d94f0884af3dd5979e11dd1d72cd
Alejandro  gallegos <alejandrogallegos@MacBook-de-Alejandro.local>
2020-07-28 21:43:37 -0500

    Se agregan primeros cambios de css

--- src/component/BrandItem.js
+++ src/component/BrandItem.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const BrandItem = (props) => {
   return (
-    <>
+    <div>
       <Link to={`/campaign/${props.data.name}`}>
         <img src={props.data.img} />
       </Link>
-    </>
+    </div>
   );
 }
 export default BrandItem
--- src/component/BrandList.js
+++ src/component/BrandList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import BrandItem from "./BrandItem";
+import "../css/BrandList.css";
 
 const BrandList = (props) => {
   return (
-    <>
+    <div className="brand-list">
       {
         props.data.map((item) => {
           return <BrandItem data={item} />
         })
       }
-    </>
+    </div>
   );
 }
 export default BrandList
