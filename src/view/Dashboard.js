bac1795f00336935457937de9c788f8fff4192de
Alejandro  gallegos <alejandrogallegos@MacBook-de-Alejandro.local>
2020-07-29 23:19:52 -0500

    Se agregan cambios en el Dashboard

--- src/view/Dashboard.js
+++ src/view/Dashboard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
+import '../css/Dashboard.css';
 
 const data = [
   {
@@ -31,22 +32,26 @@ const data = [
 const Dashboard = () => {
 
   return (
-    <LineChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5, right: 30, left: 20, bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
+    <div className="divDashboard">
+      <h1>Grafica de proyeccion</h1>
+      <h2>Aqui podemos visualizar el comportamiento de la campaña.</h2>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    </div>
   );
 }
 
